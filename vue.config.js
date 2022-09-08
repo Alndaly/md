08926efd64ac719aa1027c799238a2be7d9888a8
华勤达 <hqd01334772@antgroup.com>
2022-09-08 14:47:34 +0800

    fix: 🐛 修复白屏问题
    修改vue.config.path中的publicPath路径

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const isProd = process.env.NODE_ENV === `production`
 
 module.exports = {
   lintOnSave: true,
-  publicPath: process.env.SERVER_ENV === `NETLIFY` ? `/` : `/md/`, // 基本路径, 建议以绝对路径跟随访问目录
+  // 注意如果项目不是在服务器的根路径上，那么此处publicPath必须修改成对应的项目路径
+  publicPath: process.env.SERVER_ENV === `NETLIFY` ? `/` : `/editor/`, // 基本路径, 建议以绝对路径跟随访问目录
   configureWebpack: (config) => {
     config.module.rules.push({
       test: /\.(txt|md)$/i,
